df093ea6ca4348dddc9417576d30f40e4f0594e4
unluckycord <loganknapp2@gmail.com>
2022-05-10 16:59:48 -0700

    added comments to explain what does what

--- main.java
+++ main.java
@@ -4,9 +4,16 @@ public class main{
     public static boolean run = true;
     public static void main(String[] args) throws IOException {
         wordStorage.loadingWords();
-        System.out.println(wordStorage.wordbank);
+        degubbing();
         //while(run){
 
         //}
+    }
+    // invoke this class to test code out
+    public static void degubbing(){
+        if(wordStorage.wordbank.contains("APPLE")){
+            System.out.println("contains apple");
+        }
+        System.out.println(wordStorage.wordbank);
     }
 }
--- wordStorage.java
+++ wordStorage.java
@@ -3,8 +3,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class wordStorage {
+    
+    //this is the ArrayList that holds the words
+    //to reference it, use wordStroage.wordbank
+
     public static ArrayList<String> wordbank = new ArrayList<String>();
+    
+    //this method grabs words from the dictionary
+    //and stores them in an ArrayList
+    
     public static void loadingWords() throws IOException{
         try(BufferedReader br = new BufferedReader(new FileReader("dictionary.txt"))){
             String line = br.readLine();
@@ -12,7 +21,6 @@ public class wordStorage {
                 wordbank.add(line);
                 line = br.readLine();
             }
-
         }
     }
 }
