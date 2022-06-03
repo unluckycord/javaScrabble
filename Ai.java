861d67014cdd277f865f24471c6389affee6e754
VmVKyrie <nekomata357@gmail.com>
2022-06-03 14:29:39 -0700

    added more to Ai java

--- Ai.java
+++ Ai.java
@@ -36,3 +36,30 @@ public class Ai {
     }
 
 }
+
+/*
+ * public class Ai {
+ * String name;
+ * public Ai(){
+ * name = "";
+ * }
+ * 
+ * public void setName(String newName){
+ * name = newName;
+ * }
+ * 
+ * import java.util.Arrays;
+ * import java.util.Collections;
+ * import java.util.List;
+ * 
+ * public class AiNames {
+ * public static void main(String[] args) {
+ * String[] peoples = {"Bob","Jill","Tom","Brandon"};
+ * List<String> names = Arrays.asList(peoples);
+ * Collections.shuffle(names);
+ * for (String name : names) {
+ * System.out.print(name + " ");
+ * }
+ * }
+ * }
+ */
