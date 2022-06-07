3e259554497a6395b34dc17e6b0efc83e738cea1
unluckycord <loganknapp2@gmail.com>
2022-06-07 14:45:11 -0700

    adding comments

--- wordStorage.java
+++ wordStorage.java
@@ -13,6 +13,9 @@ public class wordStorage {
     // this method grabs words from the dictionary
     // and stores them in an ArrayList
 
+
+    // code modified from stackoverflow
+    //https://stackoverflow.com/questions/19844649/java-read-file-and-store-text-in-an-array
     public static void loadingWords() throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader("Assets/dictionary.txt"))) {
             String line = br.readLine();
